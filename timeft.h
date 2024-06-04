27f091b57a02613018956651fb3a2b3213f2b71b
YuMinha <ul8822@kumoh.ac.kr>
2024-06-05 02:54:22 +0900

    upload

--- timeft.h
+++ timeft.h
@@ -1,11 +1,5 @@
-#include <sys/time.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <semaphore.h>
-#include "wrapper.h"
-
-void init_timelog(int size);
-void start_timelog(int idx);
-void finish_timelog(int idx);
-void close_timelog(void);
+extern void init_timelog(int size);
+extern void start_timelog(int idx);
+extern void finish_timelog(int idx);
+extern void close_timelog(void);
 
